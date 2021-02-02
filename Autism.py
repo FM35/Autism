bd27a55d4fe01d4c681c66cff05dcdfa589806a6
Fungai Mawoyo <fungaimawoyo@gmail.com>
2021-02-02 14:33:45 -0700

    Just finished balancing the dataset

--- Autism.py
+++ Autism.py
@@ -1,3 +1,5 @@
+import numpy as np
+from sklearn import preprocessing
 from scipy.io import arff
 import pandas as pd
 
@@ -12,3 +14,24 @@ for x in inputs:
 Autism_unprocessed = Autism_unprocessed.drop(['ethnicity','relation', 'age_desc'], axis = 1)
 Autism_unprocessed = Autism_unprocessed.dropna(axis = 0)
 Autism_unprocessed = Autism_unprocessed[Autism_unprocessed.age != 383]
+Autism_unprocessed['Class/ASD'] = Autism_unprocessed['Class/ASD'].map({'YES': 1,'NO': 0})
+
+Autism_unprocessed = Autism_unprocessed.reset_index(drop = True)
+unscaled_inputs = Autism_unprocessed.drop(['Class/ASD'], axis = 1)
+targets = Autism_unprocessed['Class/ASD']
+
+print(Autism_unprocessed['used_app_before'].value_counts())
+
+
+num_one_targets = int(np.sum(targets))
+zero_targets_counter = 0
+indices_to_remove = []
+
+for i in range(targets.shape[0]):
+    if targets[i] ==0:
+        zero_targets_counter += 1
+        if zero_targets_counter > num_one_targets:
+            indices_to_remove.append(i)
+
+unscaled_inputs_equal_priors = unscaled_inputs.drop(indices_to_remove)
+targets_equal_priors = targets.drop(indices_to_remove)
